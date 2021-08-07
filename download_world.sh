96383494a0c71ae28d0995c2ca4021da3ce1453e
Kenta Suzuki <k-suzuki@mi-labo.co.jp>
2021-08-07 19:09:52 +0900

    #2 bashの比較演算は -eq

--- download_world.sh
+++ download_world.sh
@@ -16,23 +16,23 @@ status=$?
 # 1:おそらくトークンが古い
 # 2:おそらく中に人がいてバックアップが取れない
 # 3:ワールドデータがダウンロードできなかった
-if [ $status -e 1 ];then
+if [ $status -eq 1 ];then
     # 1の場合はトークンを取得し直して再試行
     echo "MCMAP> [RENEW TOKEN]"
     source renew-credentials.sh
     minecraft-tools/realms-download.sh
     status=$?
-    if [ $status -e 1 ];then
+    if [ $status -eq 1 ];then
         echo "MCMAP> [ERROR: REALMS ACCESS FAILED]"
         notice "再ログインしたけどダウンロード失敗した。。"
         exit 1
     fi
 fi
-if [ $status -e 2 ];then
+if [ $status -eq 2 ];then
     echo "MCMAP> [ERROR: BACKUP TAKING FAILED]"
     exit 2
 fi
-if [ $status -e 3 ];then
+if [ $status -eq 3 ];then
     echo "MCMAP> [ERROR: DOWNLOAD FAILED]"
     notice "バックアップまでは取得できたっぽいけど何故かダウンロードできなかった。。"
     exit 3
